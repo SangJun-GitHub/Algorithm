3c3d5304350dc8c47073ef98d4bb29ca6c954d54
Sang Jun Park <sjpark24@myseneca.ca>
2020-11-13 22:51:47 -0500

    commit

---
+++ BinarySearch/P1.java
@@ -0,0 +1,44 @@
+package BinarySearch;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Created by Sang Jun Park on 10/22/2020.
+ * Github : http://github.com/SangJun-GitHub
+ **/
+//https://www.acmicpc.net/problem/1920
+
+
+public class P1 {
+    static int[] A;
+    public static int binarySearch(int []array, int key){
+        int start = 0;
+        int end = array.length -1;
+        while(start <= end){
+            int mid = start + (end - start) /2;
+            if(key < array[mid])
+                end = mid -1;
+            else if(key > array[mid])
+                start = mid + 1;
+            else
+                return 1;
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        A = new int[input];
+        for(int i = 0; i < input; i++){
+            A[i] = sc.nextInt();
+        }
+        Arrays.sort(A);
+        int input2 = sc.nextInt();
+        int number;
+        for(int i = 0 ; i < input2; i++){
+            number = sc.nextInt();
+            System.out.println(binarySearch(A,number));
+        }
+    }
+}
