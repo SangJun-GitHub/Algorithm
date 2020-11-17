cef8762d90dc4594176b19bd95cf6fcf504294fd
Sang Jun Park <sjpark24@myseneca.ca>
2020-11-16 20:00:36 -0500

    commit

---
+++ BinarySearch/P9.java
@@ -0,0 +1,43 @@
+package BinarySearch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by Sang Jun Park on 11/16/2020.
+ * Github : http://github.com/SangJun-GitHub
+ */
+public class P9 {
+    static long S;
+    public static long binarySearch(long s){
+        long start = 1;
+        long end = s;
+        long answer = 0;
+        while(start <= end){
+            long mid = (start + end) / 2;
+            long sum = 0;
+            boolean over = false;
+            for(int i = 1; i <=mid; i++){
+                sum += i;
+                if(sum > s){
+                    end = mid -1;
+                    over = true;
+                    break;
+                }
+            }
+            if(sum == s){
+                return mid;
+            }else if(!over){
+                answer = mid;
+                start = mid + 1;
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        S = Long.parseLong(br.readLine());
+        System.out.println(binarySearch(S));
+    }
+}
