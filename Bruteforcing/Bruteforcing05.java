587559966b3e6962cdd4681c7b3ee76487c043a7
Sang Jun Park <sjpark24@myseneca.ca>
2020-11-19 20:20:41 -0500

    commit

---
+++ Bruteforcing/Bruteforcing05.java
@@ -0,0 +1,11 @@
+package Bruteforcing;
+
+/**
+ * Created by Sang Jun Park on 11/19/2020.
+ * Github : http://github.com/SangJun-GitHub
+ */
+public class Bruteforcing05 {
+    public static void main(String[] args) {
+        
+    }
+}
