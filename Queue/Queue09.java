0ef2b5306400a0ba62c0ae4e5a7daa15dcf62421
Sang Jun Park <sjpark24@myseneca.ca>
2020-12-10 23:32:27 -0500

    commit

--- Queue/Queue09.java
+++ Queue/Queue09.java
@@ -4,8 +4,5 @@ package Queue;
  * Created by Sang Jun Park on 2020-12-10.
  * Github : http://github.com/SangJun-GitHub
  */
-public class Queue08 {
-    public static void main(String[] args) {
-
-    }
+public class Queue09 {
 }
