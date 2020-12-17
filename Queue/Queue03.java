543852c518d4a3f6ea05c1da3229142efa9f4f2b
Sang Jun Park <sjpark24@myseneca.ca>
2020-12-16 23:38:57 -0500

    commit

--- Queue/Queue03.java
+++ Queue/Queue03.java
@@ -4,6 +4,7 @@ package Queue;
  * Created by Sang Jun Park on 2020-12-04.
  * Github : http://github.com/SangJun-GitHub
  */
+//https://www.acmicpc.net/problem/1966
 public class Queue03 {
     public static void main(String[] args) {
 
