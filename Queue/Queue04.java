a9971969530ad9a184e04de9c4515736e6505bee
Sang Jun Park <sjpark24@myseneca.ca>
2020-12-17 23:07:10 -0500

    commit

--- Queue/Queue04.java
+++ Queue/Queue04.java
@@ -4,7 +4,7 @@ package Queue;
  * Created by Sang Jun Park on 2020-12-07.
  * Github : http://github.com/SangJun-GitHub
  */
-////https://www.acmicpc.net/problem/3078
+//https://www.acmicpc.net/problem/3078
 public class Queue04 {
     public static void main(String[] args) {
         
--- Queue/Queue05.java
+++ Queue/Queue05.java
@@ -4,6 +4,7 @@ package Queue;
  * Created by Sang Jun Park on 2020-12-08.
  * Github : http://github.com/SangJun-GitHub
  */
+//https://www.acmicpc.net/problem/10866
 public class Queue05 {
     public static void main(String[] args) {
 
