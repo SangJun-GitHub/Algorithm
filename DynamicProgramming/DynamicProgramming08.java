51b03be801818f2ea82c34f885d739f3d85e944e
Sang Jun Park <sjpark24@myseneca.ca>
2020-12-21 21:23:17 -0500

    commit

--- DynamicProgramming/DynamicProgramming08.java
+++ DynamicProgramming/DynamicProgramming08.java
@@ -1,12 +1,28 @@
 package DynamicProgramming;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created by Sang Jun Park on 2020-12-01.
  * Github : http://github.com/SangJun-GitHub
  */
 //https://www.acmicpc.net/problem/2294
 public class DynamicProgramming08 {
-    public static void main(String[] args) {
-
+    static int[] coin;
+    static int[] dp;
+    static int N;
+    static int K;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] line = br.readLine().split(" ");
+        N = Integer.parseInt(line[0]);
+        K = Integer.parseInt(line[1]);
+        coin = new int[N];
+        for(int i = 0; i < N; i++){
+            coin[i] = Integer.parseInt(br.readLine());
+        }
+        dp = new int[K + 1];
     }
 }
