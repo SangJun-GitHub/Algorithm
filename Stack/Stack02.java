ecedc470ccb8ec1b60d82dc944554ebea8cc1328
Sang Jun Park <sjpark24@myseneca.ca>
2020-12-22 23:14:48 -0500

    commit

--- Stack/Stack02.java
+++ Stack/Stack02.java
@@ -4,8 +4,5 @@ package Stack;
  * Created by Sang Jun Park on 12/22/2020.
  * Github : http://github.com/SangJun-GitHub
  */
-public class Stack01 {
-    public static void main(String[] args) {
-
-    }
+public class Stack02 {
 }
