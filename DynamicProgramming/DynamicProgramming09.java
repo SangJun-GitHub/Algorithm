4e2a850382a9d5ff115dd08eb59eab6e107e06ad
Sang Jun Park <sjpark24@myseneca.ca>
2020-12-23 22:58:13 -0500

    commit

--- DynamicProgramming/DynamicProgramming09.java
+++ DynamicProgramming/DynamicProgramming09.java
@@ -11,9 +11,18 @@ import java.io.InputStreamReader;
 //https://www.acmicpc.net/problem/1699
 public class DynamicProgramming09 {
     static int N;
+    static int[] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        
+        dp = new int[N + 1];
+        dp[1] = 1;
+        for(int i = 2; i <= N; i++){
+            dp[i] = i;
+            for(int j = 1; j * j <= i; j++){
+                dp[i] = Math.min(dp[i-(j*j)] + 1, dp[i]);
+            }
+        }
+
     }
 }
