4bd9dc862b85276ac716937204775f08b9c36896
Sang Jun Park <sjpark24@myseneca.ca>
2020-12-23 22:41:00 -0500

    commit

--- DynamicProgramming/DynamicProgramming09.java
+++ DynamicProgramming/DynamicProgramming09.java
@@ -1,12 +1,19 @@
 package DynamicProgramming;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created by Sang Jun Park on 2020-12-02.
  * Github : http://github.com/SangJun-GitHub
  */
 //https://www.acmicpc.net/problem/1699
 public class DynamicProgramming09 {
-    public static void main(String[] args) {
-
+    static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        
     }
 }
