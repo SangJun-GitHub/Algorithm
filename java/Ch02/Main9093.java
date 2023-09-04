4f0013be2e0fffe8a79573e9af797517a5e66aa5
sangjunpark <bobsang89@gmail.com>
2023-09-04 13:12:14 -0400

    commit

--- java/Ch02/Main9093.java
+++ java/Ch02/Main9093.java
@@ -1,8 +1,34 @@
 package Ch02;
 
+import java.io.*;
+import java.util.Stack;
+
 /**
  * Created by Sang Jun Park on 9/1/2023.
  * Github : http://github.com/SangJun-GitHub
  */
 public class Main9093 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+
+        while(T-- > 0){
+            Stack<Character> stack = new Stack<>();
+            String str = br.readLine() + '\n';
+            for(char ch : str.toCharArray()){
+                if(ch == ' ' || ch =='\n'){
+                    while(!stack.empty()){
+                        bw.write(stack.pop());
+                    }
+                    bw.write(ch);
+                } else {
+                    stack.push(ch);
+                }
+            }
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
