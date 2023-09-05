dfb700fd313cbefef7b435c2c17ab911dab47e7c
sangjunpark <bobsang89@gmail.com>
2023-09-05 00:18:09 -0400

    commit

---
+++ java/Ch02/Main1406.java
@@ -0,0 +1,47 @@
+package Ch02;
+
+import java.io.*;
+import java.util.Stack;
+
+public class Main1406 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        Stack<Character> left = new Stack<>();
+        Stack<Character> right = new Stack<>();
+
+        String line = br.readLine();
+        int M = Integer.parseInt(br.readLine());
+        for(char c : line.toCharArray()){
+            left.push(c);
+        }
+
+        while(M-- > 0){
+            String x = br.readLine();
+            if(x.charAt(0) == 'L'){
+                if(!left.empty()) {
+                    right.push(left.pop());
+                }
+            } else if(x.charAt(0) == 'D'){
+                if(!right.empty()) {
+                    left.push(right.pop());
+                }
+            } else if(x.charAt(0) == 'B'){
+                if(!left.empty()) {
+                    left.pop();
+                }
+            } else if(x.charAt(0) == 'P'){
+                left.push(x.charAt(2));
+            }
+        }
+        while(!left.empty()){
+            right.push(left.pop());
+        }
+        while (!right.empty()){
+            bw.write(right.pop());
+        }
+        bw.flush();
+        bw.close();
+    }
+}
