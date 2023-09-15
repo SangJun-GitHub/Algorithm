fea84c4d530675db0c77506c6ed63cde9bc0b1fd
sangjunpark <bobsang89@gmail.com>
2023-09-14 23:56:23 -0400

    commit

--- java/Ch3/Main17087.java
+++ java/Ch3/Main17087.java
@@ -1,9 +1,42 @@
 package Ch3;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 /**
  * Created by Sang Jun Park on 09/13/2023.
  * GitHub : http://github.com/SangJun-GitHub
  */
 
 public class Main17087 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        long[] distances = new long[N];
+        long S = Long.parseLong(st.nextToken());
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0; i < N; i++){
+            long A = Long.parseLong(st.nextToken());
+            distances[i] = Math.abs(S - A);
+        }
+
+        long result = distances[0];
+        for(int i = 1; i < N ; i++){
+             result = GCD(result, distances[i]);
+    
+        }
+
+        System.out.println(result);
+    }
+    public static long GCD(long a, long b){
+        if(b == 0){
+            return a;
+        }
+        return GCD(b, a % b);
+    }
 }
