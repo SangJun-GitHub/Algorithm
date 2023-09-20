4b951a7748af0d01db68e612a0e7c8e5e1f1bda6
sangjunpark <bobsang89@gmail.com>
2023-09-20 00:37:50 -0400

    commit

--- java/Ch04/Main2193.java
+++ java/Ch04/Main2193.java
@@ -1,9 +1,42 @@
 package Ch04;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created by Sang Jun Park on 09/17/2023.
  * GitHub : http://github.com/SangJun-GitHub
  */
-
+//https://www.acmicpc.net/problem/2193
 public class Main2193 {
+    static long[][] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        dp = new long[N + 1][2];
+        System.out.println(bottomUp(N));
+    }
+    public static long bottomUp(int n){
+        dp[1][0] = 0;
+        dp[1][1] = 1;
+
+        for(int i = 2; i <= n; i++){
+            for(int j = 0; j <= 1; j++){
+                if(j == 0){
+                    dp[i][j] = dp[i - 1][0] + dp[i - 1][1];
+                }else{
+                    dp[i][j] += dp[i - 1][0];
+                }
+            }
+        }
+
+        long result = 0;
+        for(int i = 0; i <=1; i++){
+            result += dp[n][i];
+        }
+
+        return result;
+    }
 }
