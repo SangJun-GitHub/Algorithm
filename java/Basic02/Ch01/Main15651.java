219e94727f7f17d378b6d6b78db9eadfe8529f1e
sangjunpark <bobsang89@gmail.com>
2023-10-10 22:07:58 -0400

    commit

---
+++ java/Basic02/Ch01/Main15651.java
@@ -0,0 +1,44 @@
+package Basic02.Ch01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * Created by Sang Jun Park on 10/09/2023.
+ * GitHub : http://github.com/SangJun-GitHub
+ */
+//https://www.acmicpc.net/problem/15651
+public class Main15651 {
+    static int[] array = new int[9];
+    static int N;
+    static int M;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        bruteForce(0, N, M);
+        System.out.println(sb);
+    }
+    public static void bruteForce(int index, int n, int m){
+        if(index == m){
+            for(int i = 0; i < m; i++){
+                sb.append(array[i]);
+                if(i != m -1){
+                    sb.append(" ");
+                }
+            }
+            sb.append("\n");
+            return;
+        }
+        for(int i = 1; i <= n; i++){
+            array[index] = i;
+            bruteForce(index + 1, n, m);
+        }
+    }
+}
